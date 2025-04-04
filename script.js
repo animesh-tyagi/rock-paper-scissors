49f5118cc7196b1f8a8850b0cac9383df1690074
animesh-tyagi <animesh.tyagi.720@gmail.com>
2025-04-04 19:19:42 +0000

    Added UI

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
+let playerScore;
+let computerScore;
+
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function getHumanChoice() {
-    let input = prompt("Enter rock, paper or scissors: ");
-    switch (input) {
-        case "rock":
-        case "ROCK":
-        case "Rock":
-            return 0;
-
-        case "paper":
-        case "PAPER":
-        case "Paper":
-            return 1;
-
-        case "scissors":
-        case "SCISSORS":
-        case "Scissors":
-            return 2;
-
-        default:
-            console.log("Not a valid option.");
-            break;
-    }
-}
-
-
-
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
+
         console.log("Both played the same, no winner this round.");
         return;
     } else if(humanChoice === 0 && computerChoice === 1) {
@@ -59,25 +37,12 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function playGame() {
-    humanScore = 0;
-    computerScore = 0;
-    for(let i=0; i<5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    if(humanScore > computerScore) {
-        console.log("You won!");
-        console.log(`Final scores: \nYou - ${humanScore} \nComputer - ${computerScore}`);
-    } else if(computerScore > humanScore) {
-        console.log("You lost!");
-        console.log(`Final scores: \nYou - ${humanScore} \nComputer - ${computerScore}`);
-    } else {
-        console.log("It's a tie!");
-        console.log(`Final scores: \nYou - ${humanScore} \nComputer - ${computerScore}`);
-    }
-}
-let humanScore;
-let computerScore;
-playGame();
+const rock = document.querySelector("rock");
+rock.addEventListener("click", playRound(0,() => getComputerChoice()));
+
+const paper = document.querySelector("paper");
+paper.addEventListener("click", playRound(1,() => getComputerChoice()));
+
+const scissors = document.querySelector("scissors");
+scissors.addEventListener("click", playRound(2,() => getComputerChoice()));
+
